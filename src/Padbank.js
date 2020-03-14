405f6e85f09f10fc743a7aa70c9fa480ecd9232c
wardDes <dar-el@outlook.com>
2020-03-13 18:28:39 -0700

    implemented PadBank and added import for DrumPad component

---
+++ src/Padbank.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import DrumPad from './DrumPad'
+
+const Padbank = props => {
+
+    let padBank;
+
+    props.power ? 
+        padBank = props.currentPadBank.map((drumObj, i , padBankArr) => {
+            return (
+                <DrumPad
+                    clipId = {padBankArr[i].id}
+                    clip={padBankArr[i].url}
+                    keyTrigger={padBankArr[i].keyTrigger}
+                    keyCode={padBankArr[i].keyCode}
+                    updateDisplay={props.updateDisplay}
+                    power={props.power} />
+            )
+        }) :
+        padBank = props.currentPadBank.map((drumObj, i , padBankArr) => {
+            return (
+                <DrumPad
+                    clipId = {padBankArr[i].id}
+                    clip="#"
+                    keyTrigger={padBankArr[i].keyTrigger}
+                    keyCode={padBankArr[i].keyCode}
+                    updateDisplay={props.updateDisplay}
+                    power={props.power} />
+            )
+        })
+    return (
+        <div className="pad-bank">
+            {padBank}
+        </div>
+    )
+}
+
+export default Padbank
